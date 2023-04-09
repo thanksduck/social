60e4b238b9e1d470ac018948b4530a087eb1ceac
sivm99 <git.v7gr4@simplelogin.fr>
2023-04-09 06:18:30 +0530

    some bekar changes were made

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const imgs = document.querySelectorAll('img');
-
-imgs.forEach(img => {
-	img.addEventListener('error', () => {
-		img.style.display = 'none';
-	});
-});
+// This is an optional step. You can remove it if you don't need it.
+// It's used to preload the SVG images to make sure they are loaded before they are displayed.
+const images = [
+    'assets/telegram.svg',
+    'assets/instagram.svg',
+    'assets/github.svg',
+    'assets/snapchat.svg',
+    'assets/twitter.svg',
+  ];
+  
+  for (const image of images) {
+    const img = new Image();
+    img.src = image;
+  }
+  
